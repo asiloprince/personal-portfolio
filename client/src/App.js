5c4e3878d8cc0fa245bb4ace6b1e316ce735cd04
asiloprince <asilo.prince25@gmail.com>
2022-10-29 13:55:52 +0800

    fix some components

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,30 @@
-import './App.css';
-import Navigation from './screens/navigationBar/NavigationBar';
-import ParticleAnimation from 'react-particle-animation'
-import Header from './screens/header/Header';
-import AboutMe from './screens/aboutMe/AboutMe';
-import Technologies from './screens/technologies/Technologies';
-import ContactMe from './screens/contactMe/ContactMe';
-import Portfolio from './screens/portfolio/Portfolio';
-import Footer from './screens/footer/Footer';
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
-
+import "./App.css";
+import Navigation from "./pages/navigationBar/NavigationBar";
+import ParticleAnimation from "react-particle-animation";
+import Header from "./pages/header/Header";
+import AboutMe from "./pages/aboutMe/AboutMe";
+import Technologies from "./pages/technologies/Technologies";
+import ContactMe from "./pages/contactMe/ContactMe";
+import Portfolio from "./pages/portfolio/Portfolio";
+import Footer from "./pages/footer/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Header />
+
       <ParticleAnimation
-        style={{ height: '94vh' }}
+        style={{ height: "94vh" }}
         background={{ r: 4, g: 4, b: 44, a: 1 }}
         numParticles={60}
         particleSpeed={0.3}
         interactive={true}
         particleRadius={1.5}
       />
+
       <AboutMe />
       <Portfolio />
       <Technologies />
